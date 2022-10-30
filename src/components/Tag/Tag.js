6e5d6d35f53d12ea7ae7e5aa9635b18cec50b384
Roman Gorev <ogogorev@yandex.ru>
2022-10-30 21:15:47 +0100

    Fix renaming folders

---
+++ src/components/Tag/Tag.js
@@ -0,0 +1,12 @@
+const { css } = require("styled-components");
+
+export const baseTagStyles = css`
+  font-size: 13px;
+  font-weight: 700;
+  color: var(--primary);
+  background-color: var(--grayish-cyan);
+  border: none;
+  border-radius: var(--border-radius);
+
+  padding: 8px;
+`;
